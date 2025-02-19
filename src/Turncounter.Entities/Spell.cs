6977a973b889b8330d658b009f635bfd3d282ebc
Wellan21 <tristan.marcoux21@gmail.com>
2025-02-19 20:44:53 +0100

    Add base entities

---
+++ src/Turncounter.Entities/Spell.cs
@@ -0,0 +1,33 @@
+﻿namespace Turncounter.Entities
+{
+    /// <summary>
+    /// Classe représentant un sort.
+    /// </summary>
+    class Spell
+    {
+        /// <summary>
+        /// Identifiant unique du sort.
+        /// </summary>
+        public int SpellId { get; set; }
+
+        /// <summary>
+        /// Nom du sort.
+        /// </summary>
+        public string SpellName { get; set; }
+
+        /// <summary>
+        /// Coût en mana du sort.
+        /// </summary>
+        public int Mana { get; set; }
+
+        /// <summary>
+        /// Temps de recharge du sort.
+        /// </summary>
+        public int SpellCuntdown { get; set; }
+
+        /// <summary>
+        /// Tours restants avant que le sort puisse être réutilisé.
+        /// </summary>
+        public int RemainingTurn { get; set; }
+    }
+}
---
+++ src/Turncounter.Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Turncounter.Entities
+{
+    public class User
+    {
+        int IdUser { get; set; }   
+        string Username { get; set; }
+        string Password { get; set; }   
+        IEnumerable<Character> Characters { get; set; }
+
+    }
+}
